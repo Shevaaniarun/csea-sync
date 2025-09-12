e4e8ccb2fa15b3c8fff2739955411fb16127abb9
Sripramod Yogarajan <ysripramod@gmail.com>
2025-09-13 03:25:10 +0530

    feat: implement footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { EventsSection } from "./components/events/EventsSection";
+import { Footer } from "./components/Footer";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-background">
       <EventsSection />
+      <Footer />
     </div>
   );
 }
