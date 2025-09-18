6e0aaae1a0b23528ee659ca5ab2ab18e60290bbb
Shevaaniarun <shevaaniarun@gmail.com>
2025-09-18 09:00:18 +0530

    folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { EventsSection } from "./components/events/EventsSection";
 import AppLoader from "./components/ui/loader/AppLoader";
-import LightRays from "./components/ui/LightRays";
-import ParticlesBackground from "./components/ui/ParticlesBackground";
+import LightRays from "./components/ui/bg-animations/LightRays";
+import ParticlesBackground from "./components/ui/bg-animations/ParticlesBackground";
 import { Footer } from "./components/Footer";
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
