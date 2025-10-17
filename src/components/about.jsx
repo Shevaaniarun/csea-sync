fc9f4896d9930638b5bf7338065345d567881fa1
cseatech <cegcseatech@gmail.com>
2025-10-17 20:10:36 +0530

    corrected sync date

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -40,11 +40,11 @@ export default function AboutUs() {
 
           <p className="text-gray-200 leading-relaxed text-sm sm:text-base">
             This year, we have decided to have the events around Digital
-            Well-being. Sync ‘24 is about to blossom afresh on 27th and 28th of
-            September, 2024. Encompassing multifarious events, this version has
+            Well-being. Sync ‘25 is about to blossom afresh on 24th and 25th of
+            October, 2025. Encompassing multifarious events, this version has
             the benchmark features to enthrall the multitude, albeit in a
             sustainable manner. Every experience brings a greater clarity of
-            vision and thus with the past experiences, Sync ‘24 is all set to
+            vision and thus with the past experiences, Sync ‘25 is all set to
             reach further horizons.
           </p>
         </div>
