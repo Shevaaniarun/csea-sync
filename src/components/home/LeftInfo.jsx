eb47064b69482a7c10ed4ac3a70bc930133875f9
cegcsea <cseaceg.tech@gmail.com>
2025-10-18 14:00:28 +0530

    changed dates

--- src/components/home/LeftInfo.jsx
+++ src/components/home/LeftInfo.jsx
@@ -6,7 +6,7 @@ const LeftInfo = () => {
     <div className="about-container">
       {/* The H1 now gets its blue color from the CSS class directly for consistency */}
       <h1 className="font-semibold text-cyan-200 glowing-title">Sync '25</h1>
-      <p>24th and 25th September, 2025</p>
+      <p>25th and 26th September, 2025</p>
       <p className="animated-slogan">Disconnect 🔌 to Reconnect ✨</p>
     </div>
   );
